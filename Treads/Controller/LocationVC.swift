86321ed974977ab60a9f8a4cd8be6379f9bd5172
Khánh Minh <minhbi245@gmail.com>
2019-09-16 15:39:32 +0700

    map view and shared locationVC finished

---
+++ Treads/Controller/LocationVC.swift
@@ -0,0 +1,31 @@
+//
+//  LocationVC.swift
+//  Treads
+//
+//  Created by KhanhNguyen on 9/16/19.
+//  Copyright © 2019 KhanhNguyen. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationVC: UIViewController, MKMapViewDelegate {
+
+    var manager: CLLocationManager?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        manager = CLLocationManager()
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.activityType = .fitness
+    }
+    
+    func checkLocationAuthStatus() {
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            manager?.requestWhenInUseAuthorization()
+        }
+    }
+    
+    
+
+}
