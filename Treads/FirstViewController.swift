f15197e9bb658b1e9c2dfd0ce27316ffb3458835
Khánh Minh <minhbi245@gmail.com>
2019-09-16 11:55:52 +0700

    Initial Commit

---
+++ Treads/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Treads
+//
+//  Created by KhanhNguyen on 9/16/19.
+//  Copyright © 2019 KhanhNguyen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by KhanhNguyen on 9/16/19.
+//  Copyright © 2019 KhanhNguyen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
